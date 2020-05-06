5e094dc99ea52fabfd9d287a9161c25e4843cbfc
pursuemoon <shili_xu@qq.com>
2020-05-07 00:17:01 +0800

    新增构建算法的求解质量比较的测试

--- src/test/java/org/pursuemoon/solvetsp/ga/operator/TestGeneratingOperator.java
+++ src/test/java/org/pursuemoon/solvetsp/ga/operator/TestGeneratingOperator.java
@@ -3,8 +3,13 @@ package org.pursuemoon.solvetsp.ga.operator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.pursuemoon.solvetsp.ga.Solution;
+import org.pursuemoon.solvetsp.ga.TspSolver;
+import org.pursuemoon.solvetsp.util.DataExtractor;
+import org.pursuemoon.solvetsp.util.Painter;
 
 import java.util.*;
+
+import static java.lang.Double.*;
 
 public class TestGeneratingOperator {
 
@@ -41,6 +46,61 @@ public class TestGeneratingOperator {
         ConvexHullDivisionGeneratingOperator operator = new ConvexHullDivisionGeneratingOperator(100);
         Solution solution = operator.generate();
         Assert.assertTrue(checkIfSolutionLegal(solution));
+    }
+
+    @Test
+    public void testForComparison() {
+        for (int i = 0; i < 5; ++i) {
+            DataExtractor.instance.getNextTsp();
+        }
+        ConvexHullDivisionGeneratingOperator division = new ConvexHullDivisionGeneratingOperator(100);
+        final int times = 20;
+        double minDistance = MAX_VALUE;
+        double sum = 0;
+        for (int i = 1; i <= times; ++i) {
+            Solution solution = division.generate();
+            double distance = solution.getDistance();
+            if (distance <= minDistance) {
+                minDistance = distance;
+                Painter.paint("division", false, TspSolver.getPoints(), solution);
+            }
+            sum += solution.getDistance();
+        }
+        double average = sum / times;
+//        System.out.println("\nDivision Average: " + average);
+//        System.out.println("Division Best: " + minDistance);
+
+        ConvexHullConstrictionGeneratingOperator constriction = new ConvexHullConstrictionGeneratingOperator(100, 3);
+        minDistance = MAX_VALUE;
+        sum = 0;
+        for (int i = 1; i <= times; ++i) {
+            Solution solution = constriction.generate();
+            double distance = solution.getDistance();
+            if (distance <= minDistance) {
+                minDistance = distance;
+                Painter.paint("constriction", false, TspSolver.getPoints(), solution);
+            }
+            sum += solution.getDistance();
+        }
+        average = sum / times;
+//        System.out.println("\nConstriction Average: " + average);
+//        System.out.println("Constriction Best: " + minDistance);
+
+        NearestKNeighborsGreedyGeneratingOperator nearest = new NearestKNeighborsGreedyGeneratingOperator(100, 1);
+        minDistance = MAX_VALUE;
+        sum = 0;
+        for (int i = 1; i <= times; ++i) {
+            Solution solution = nearest.generate();
+            double distance = solution.getDistance();
+            if (distance <= minDistance) {
+                minDistance = distance;
+                Painter.paint("nearest", false, TspSolver.getPoints(), solution);
+            }
+            sum += solution.getDistance();
+        }
+        average = sum / times;
+//        System.out.println("\nNearest Average: " + average);
+//        System.out.println("Nearest Best: " + minDistance);
     }
 
     private static boolean checkIfSolutionLegal(Solution solution) {
