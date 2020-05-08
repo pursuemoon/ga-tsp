55834116a5758d18bc88d0e205c1ae2a60aea276
pursuemoon <shili_xu@qq.com>
2020-05-08 14:36:37 +0800

    增加交叉算子相关的单元测试

--- src/test/java/org/pursuemoon/solvetsp/ga/operator/TestCrossoverOperator.java
+++ src/test/java/org/pursuemoon/solvetsp/ga/operator/TestCrossoverOperator.java
@@ -3,6 +3,7 @@ package org.pursuemoon.solvetsp.ga.operator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.pursuemoon.solvetsp.ga.Solution;
+import org.pursuemoon.solvetsp.util.DataExtractor;
 
 import java.util.BitSet;
 import java.util.List;
@@ -18,7 +19,7 @@ public class TestCrossoverOperator {
         Solution p1 = randomGeneratingOperator.generate();
         Solution p2 = randomGeneratingOperator.generate();
         List<Solution> offspring = operator.crossover(p1, p2);
-        for (Solution solution: offspring) {
+        for (Solution solution : offspring) {
             Assert.assertTrue(checkIfSolutionLegal(solution));
         }
         int[] geneP1 = p1.getClonedGene();
@@ -36,6 +37,17 @@ public class TestCrossoverOperator {
     }
 
     @Test
+    public void testSectionCrossoverOperator() {
+        SectionCrossoverOperator operator = new SectionCrossoverOperator(100);
+        Solution p1 = randomGeneratingOperator.generate();
+        Solution p2 = randomGeneratingOperator.generate();
+        List<Solution> offspring = operator.crossover(p1, p2);
+        for (Solution solution : offspring) {
+            Assert.assertTrue(checkIfSolutionLegal(solution));
+        }
+    }
+
+    @Test
     public void testNearestNeighborCrossoverOperator() {
         NearestNeighborCrossoverOperator operator = new NearestNeighborCrossoverOperator(100);
         Solution p1 = randomGeneratingOperator.generate();
@@ -43,6 +55,45 @@ public class TestCrossoverOperator {
         List<Solution> offspring = operator.crossover(p1, p2);
         for (Solution solution: offspring) {
             Assert.assertTrue(checkIfSolutionLegal(solution));
+        }
+    }
+
+    @Test
+    public void testForComparison() {
+        for (int i = 0; i < 5; ++i) {
+            DataExtractor.instance.getNextTsp();
+        }
+        Solution p1 = randomGeneratingOperator.generate();
+        Solution p2 = randomGeneratingOperator.generate();
+        double d = (p1.getDistance() + p2.getDistance()) / 2;
+//        System.out.println("Init Average Distance: " + d);
+
+        SectionCrossoverOperator section = new SectionCrossoverOperator(100);
+        NearestNeighborCrossoverOperator nearest = new NearestNeighborCrossoverOperator(100);
+
+        final int times = 10;
+        Solution f1 = p1;
+        Solution f2 = p2;
+        for (int i = 1; i <= times; ++i) {
+            List<Solution> offspring = section.crossover(f1, f2);
+            f1 = offspring.get(0);
+            f2 = offspring.get(1);
+            if (i % 2 == 0) {
+                d = (f1.getDistance() + f2.getDistance()) / 2;
+//                System.out.println(i + " Section Average Distance: " + d);
+            }
+        }
+
+        f1 = p1;
+        f2 = p2;
+        for (int i = 1; i <= times; ++i) {
+            List<Solution> offspring = nearest.crossover(f1, f2);
+            f1 = offspring.get(0);
+            f2 = offspring.get(1);
+            if (i % 2 == 0) {
+                d = (f1.getDistance() + f2.getDistance()) / 2;
+//                System.out.println(i + " Nearest Average Distance: " + d);
+            }
         }
     }
 
